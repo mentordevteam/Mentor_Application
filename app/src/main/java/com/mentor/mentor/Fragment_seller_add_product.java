9103f0c6a68194a243db447f8dcceabda7afb89f
mentor <mentordevteam@gmail.com>
2017-01-04 23:11:56 +0530

    11:11 PM 04/01;2017

--- app/src/main/java/com/mentor/mentor/Fragment_seller_add_product.java
+++ app/src/main/java/com/mentor/mentor/Fragment_seller_add_product.java
@@ -23,7 +23,7 @@ public class Fragment_seller_add_product extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_seller_add_product, container, false);
+        return inflater.inflate(R.layout.add_product, container, false);
     }
 
 }
